8bb3ecc06fc84a89c803fa529f0b62634ef2727a
Felisberto Alberto <felisbertoalberto183@gmail.com>
2024-02-22 17:07:58 +0000

    'update'

--- src/app.ts
+++ src/app.ts
@@ -11,10 +11,9 @@ app.use(express.json())
 app.use(router)
 
 app.use((err: Error, req:Request, res:Response, next:NextFunction )=>{
-    console.log('esta passando', err.message);
     
     if (err instanceof ServerError ) {
-        return res.status(err.status).json(err.message)
+        return res.status(err.status).json({ message: err.message})
     }
 
     return res.status(500).json({ message: 'error server'})
--- src/module/usuario/useCases/postUsuario/postUsuarioUseCase.ts
+++ src/module/usuario/useCases/postUsuario/postUsuarioUseCase.ts
@@ -13,6 +13,7 @@ class PostUsuarioUseCase {
             const existingUser = await this.usuarioRepository.findByEmail(email);
             if (existingUser) {
                 next(new ServerError('Usuario already exists', 400))
+                return
             }
 
             // Hash da senha
@@ -24,6 +25,7 @@ class PostUsuarioUseCase {
         } catch (error) {
             // Lança um erro se ocorrer algum problema
             next(new ServerError('Failed to create user', 500))
+            return
         }
     }
 }
